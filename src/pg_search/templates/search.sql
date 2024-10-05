6f53278e4879d77fb7a4f01a303726407ebd5d0a
Jon Stefansson <jonstef1667@gmail.com>
2024-10-05 14:16:04 -0700

    Modify search query
    * Returns book status
    * Removes book tags from results
    * Reorders columns for better readability

--- src/pg_search/templates/search.sql
+++ src/pg_search/templates/search.sql
@@ -2,11 +2,11 @@ SELECT books.book_id,
        books.title,
        books.year,
        books.series_rank,
-       array_agg(format('%%s, %%s', authors.name_last, authors.name_first)) as author,
-       books.tags
+       books.status,
+       array_agg(format('%%s, %%s', authors.name_last, authors.name_first)) as author
 FROM books
 LEFT JOIN book_authors ON books.book_id = book_authors.book_id
 LEFT JOIN authors ON book_authors.author_id = authors.author_id
 WHERE books.searchable @@ {{ query_parser }}('english', %(query)s)
 GROUP BY authors.author_id, books.book_id
-ORDER BY authors.author_id;
+ORDER BY authors.author_id, books.series_rank;
