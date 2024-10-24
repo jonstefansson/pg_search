a5c87c0788ec1c0155d816aa30cd770976b3addb
Jon Stefansson <jonstef1667@gmail.com>
2024-10-23 21:48:55 -0700

    Fixes author name field order

--- src/pg_search/templates/book_authors.sql
+++ src/pg_search/templates/book_authors.sql
@@ -1,6 +1,6 @@
 SELECT authors.author_id,
-       authors.name_last,
        authors.name_first,
+       authors.name_last,
        authors.year
 FROM authors
 JOIN book_authors ON authors.author_id = book_authors.author_id
